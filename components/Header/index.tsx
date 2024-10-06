3d79b772e9cb1bc4b1b258ed0f4002a3aee055c0
Vishnu Kasilya P <vishnukp21064@gmail.com>
2024-10-06 18:09:13 +0530

    href

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -140,7 +140,7 @@ const Header = () => {
                             >
                               {menuItem.submenu?.map((submenuItem, index) => (
                                 <Link
-                                  ref={submenuItem.path}
+                                  href={submenuItem.path}
                                   key={index}
                                   className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3" href={""}                                >
                                   {submenuItem.title}
