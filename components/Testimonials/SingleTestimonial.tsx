9f043a23421f6b02d5ea2b5143eca5ce53301219
Vishnu Kasilya P <vishnukp21064@gmail.com>
2024-10-06 17:37:47 +0530

    testmonial

--- components/Testimonials/SingleTestimonial.tsx
+++ components/Testimonials/SingleTestimonial.tsx
@@ -1,5 +1,7 @@
 import { Testimonial } from "@/types/testimonial";
 import Image from "next/image";
+import React from "react"; // Import React to use React types
+
 const starIcon = (
   <svg width="18" height="16" viewBox="0 0 18 16" className="fill-current">
     <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z" />
@@ -9,7 +11,8 @@ const starIcon = (
 const SingleTestimonial = ({ testimonial }: { testimonial: Testimonial }) => {
   const { star, name, image, content, designation } = testimonial;
 
-  let ratingIcons = [];
+  // Explicitly define the type of ratingIcons as React.ReactNode[]
+  const ratingIcons: React.ReactNode[] = [];
   for (let index = 0; index < star; index++) {
     ratingIcons.push(
       <span key={index} className="text-yellow">
