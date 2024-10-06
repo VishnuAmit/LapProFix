a9ec8f54ce5b4dcffefaa26945959859538d5e2d
Vishnu Kasilya P <vishnukp21064@gmail.com>
2024-10-06 19:15:47 +0530

    lightmode

--- components/Header/ThemeToggler.tsx
+++ components/Header/ThemeToggler.tsx
@@ -4,7 +4,7 @@
 //   const { theme, setTheme } = useTheme();
 //   return (
 //     <button aria-label='theme toggler'
-//       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+//       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
 //       className="flex items-center justify-center text-black rounded-full cursor-pointer bg-gray-2 dark:bg-dark-bg h-9 w-9 dark:text-white md:h-14 md:w-14"
 //     >
 //       <svg
@@ -40,4 +40,4 @@
 //   );
 // };
 
-// export default ThemeToggler;
+// export default ThemeToggler;
